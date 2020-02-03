f9309affa843133844a1d3ff64f0cdd091845541
mohamed sharaf <sharaf@4Deve.local>
2020-02-03 18:09:47 +0200

    send Topic

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,22 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp()
-
 exports.sendNotification = functions.firestore
   .document('messages/{groupId1}/{groupId2}/{message}')
   .onCreate((snap, context) => {
     console.log('----------------start function--------------------')
-
     const doc = snap.data()
     console.log('xxxxxx');
     console.log(doc)
-
     const idFrom = doc.idFrom
     const idTo = doc.idTo
     const contentMessage = doc.content
+    const nameFrom = doc.nameFrom
+    const threadId = doc.threadId
     // const data = doc.data
-
     // Get push token user to (receive)
-    admin
+    if(idTo != '') {
+      admin
       .firestore()
       .collection('users')
       .where('id', '==', idTo)
@@ -54,7 +53,6 @@ exports.sendNotification = functions.firestore
                     .catch(error => {
                       console.log('Error sending message:', error)
                     })
-
                     
                
                 })
@@ -64,45 +62,61 @@ exports.sendNotification = functions.firestore
           }
         })
       })
-    return null
-  })
-
-
-  //////////////// group message notification /////////////////
-
-  exports.sendGroupNotification = functions.firestore
-  .document('groupMessages/{groupId1}/{groupId2}/{message}')
-  .onCreate((snap, context) => {
-    console.log('----------------start group function--------------------')
-
-    const doc = snap.data()
-    console.log('ggggggggg');
-    console.log(doc)
-
-    const idFrom = doc.idFrom
-    const contentMessage = doc.content
-    const groupId = doc.groupId
-
-    // Get push token user to (receive)
-    const payload = {
-      notification: {
-        title: `You have a message from "${idFrom}"`,
-        body: contentMessage,
-        badge: '1',
-        sound: 'default'
-      },
-      // data: data 
+    } else {
+      const payload = {
+        notification: {
+          title: `You have a message from "${nameFrom}"`,
+          body: contentMessage,
+          badge: '1',
+          sound: 'default'
+        },
+        // data: data 
+      }
+      console.log('the Thread id is :', threadId)
+  
+      admin
+        .messaging()
+        .sendToTopic(threadId, payload)
+        .then(response => {
+          console.log('Successfully sent message:', response)
+        })
+        .catch(error => {
+          console.log('Error sending group message:', error)
+        })
     }
-
-    admin
-      .messaging()
-      .sendToTopic(groupId, payload)
-      .then(response => {
-        console.log('Successfully sent message:', response)
-      })
-      .catch(error => {
-        console.log('Error sending group message:', error)
-      })
-      
     return null
+    
   })
+  //////////////// group message notification /////////////////
+  // exports.sendGroupNotification = functions.firestore
+  // .document('groupMessages/{groupId1}/{groupId2}/{message}')
+  // .onCreate((snap, context) => {
+  //   console.log('----------------start group function--------------------')
+  //   const doc = snap.data()
+  //   console.log('ggggggggg');
+  //   console.log(doc)
+  //   const idFrom = doc.idFrom
+  //   const contentMessage = doc.content
+  //   const groupId = doc.groupId
+  //   // Get push token user to (receive)
+  //   const payload = {
+  //     notification: {
+  //       title: `You have a message from "${idFrom}"`,
+  //       body: contentMessage,
+  //       badge: '1',
+  //       sound: 'default'
+  //     },
+  //     // data: data 
+  //   }
+  //   admin
+  //     .messaging()
+  //     .sendToTopic(groupId, payload)
+  //     .then(response => {
+  //       console.log('Successfully sent message:', response)
+  //     })
+  //     .catch(error => {
+  //       console.log('Error sending group message:', error)
+  //     })
+      
+  //   return null
+  // })
